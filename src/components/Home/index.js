e5f5c1155ca48eafdad4b245d23cba27162789da
Jose Canahui <jose.canahui@gmail.com>
2017-08-06 04:11:10 -0400

    fixed some minor linting errors

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -11,7 +11,7 @@ export default class Home extends Component {
   render() {
     return(
       <div className="app-Home">
-        <h1>Hi! I'm Jose.</h1>
+        <h1>Hi! I&rsquo;m Jose.</h1>
         <h1>I code, write, and study<Desktop><span> at <a href="https://vt.edu" target="__blank" ref="noopener">Virtginia Tech</a></span></Desktop>.</h1>
         <p className="app-Home-text">
           Here&rsquo;s an ungorganized collection of intensly interesting items:

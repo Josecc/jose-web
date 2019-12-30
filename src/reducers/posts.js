cd52fd9cc6434fbb6e34629517351d78b7ea9652
Jose Canahui <jose.canahui@gmail.com>
2019-12-30 06:17:46 -0500

    adds better regex for extracting description text

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -10,7 +10,7 @@ export function getPosts(state = initialState, action = {}) {
         link: curr.link,
         author: curr.creator,
         id: curr.guid,
-        description: curr.content.split(/>((?=.*[A-Z0-9])[\w(\-:),!"'\/$ ]+\.?)(?:<\/|[\.\w])*/)[1],
+        description: curr.content.split(/>((?=.*[A-Z0-9])[\w(\-:),!"' ’\/$ ]+\.?)(?:<\/|[\.\w])*/)[1],
       });
       return acc;
     }, []);

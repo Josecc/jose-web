88dc1201af7b6b19d1b1ca214d4aaadb56854cd6
Jose Canahui <jose.canahui@gmail.com>
2019-12-30 04:48:11 -0500

    add a better title

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -11,8 +11,8 @@ export default class Home extends Component {
   render() {
     return(
       <div className="app-Home">
-        <h1>Hellooo! I&rsquo;m Jose.</h1>
-        <h1>I love coding, writing, and reading. Below you'll find a collection of some thoughts and interests.</h1>
+        <h1>Hello! I&rsquo;m Jose.</h1>
+        <h1>I'm into coding, philosophy, and coffee.</h1>
         <Panel/>
         <Footer/>
       </div>

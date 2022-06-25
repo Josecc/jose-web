9e6d170edb880b0cbe421879c3782c8e2e2e1757
Jose Canahui <6405150+Josecc@users.noreply.github.com>
2022-06-25 02:25:50 -0400

    exposes public folder

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ module.exports = {
   entry: './app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/assets/static/"
   },
   context: path.join(__dirname, 'src'),
   resolve: {
